307559aa9a742472a18cbd152d86b72172314baf
Mathias Brossard <mathias@brossard.org>
2011-03-26 14:28:43 +0100

    Starting to work on serialization and UTF-8 coding

--- has_json.c
+++ has_json.c
@@ -29,6 +29,53 @@ static size_t has_json_token_testimator(const char *a, size_t l)
     return s / 2;
 }
 
+int32_t encode_utf8(int32_t codepoint, char *output)
+{
+    int i = 0;
+
+    if(codepoint < 0 || codepoint > 0x10FFFF || output == NULL) {
+        return -1;
+    }
+
+    if(codepoint < 0x80) {
+        output[i++] = (char) codepoint;
+    } else if(codepoint < 0x800) {
+        output[i++] = 0xC0 | (char) ((codepoint & 0x0007C0) >> 6);
+        output[i++] = 0x80 | (char) ((codepoint & 0x00003F));
+    } else if(codepoint < 0x10000) {
+        output[i++] = 0xE0 | (char) ((codepoint & 0x00F000) >> 12);
+        output[i++] = 0x80 | (char) ((codepoint & 0x000FC0) >> 6);
+        output[i++] = 0x80 | (char) ((codepoint & 0x00003F));
+    } else {
+        output[i++] = 0xF0 | (char) ((codepoint & 0x1C0000) >> 18);
+        output[i++] = 0x80 | (char) ((codepoint & 0x03F000) >> 12);
+        output[i++] = 0x80 | (char) ((codepoint & 0x000FC0) >> 6);
+        output[i++] = 0x80 | (char) ((codepoint & 0x00003F));
+    }
+
+    /* This version should be smaller but it's less readable. */
+    /*
+    if(codepoint < 0x80) {
+        output[i++] = (char) codepoint;
+    } else {
+        if(codepoint < 0x800) {
+            output[i++] = 0xC0 | (char) ((codepoint & 0x0007C0) >> 6);
+        } else {
+            if(codepoint < 0x10000) {
+                output[i++] = 0xE0 | (char) ((codepoint & 0x00F000) >> 12);
+            } else {
+                output[i++] = 0xF0 | (char) ((codepoint & 0x1C0000) >> 18);
+                output[i++] = 0x80 | (char) ((codepoint & 0x03F000) >> 12);
+            }
+            output[i++] = 0x80 | (char) ((codepoint & 0x000FC0) >> 6);
+        }
+        output[i++] = 0x80 | (char) ((codepoint & 0x00003F));
+    }
+    */
+
+    return i;
+}
+
 static has_t *has_json_build(jsmntok_t *tokens, size_t cur, size_t max,
                              const char *buffer, size_t *consumed)
 {
@@ -94,7 +141,6 @@ static has_t *has_json_build(jsmntok_t *tokens, size_t cur, size_t max,
     return r;
 }
 
-
 has_t *has_json_parse(const char *buffer)
 {
     jsmn_parser parser;
@@ -119,3 +165,154 @@ has_t *has_json_parse(const char *buffer)
     return r;
 }
 
+typedef struct {
+    size_t size;
+    bool encode;
+} has_json_estimate_t;
+
+int has_json_estimate_walker(has_t *cur, has_walk_t type, int index,
+                  const char *string, size_t size, has_t *element,
+                  void *pointer)
+{
+    has_json_estimate_t *e = pointer;
+
+    if(cur == NULL) {
+        return -1;
+    }
+
+    if(type == has_walk_hash_begin) {
+        e->size += 2 +                      /* '{' and '}'             */
+            + ( cur->value.hash.count ) * 4 /* key quotes, ':' and ',' */
+            - 1;                            /* no ',' after last entry */
+    } else if(type == has_walk_hash_key) {
+        e->size += size;
+        /* FIXME: handle string encoding case */
+    } else if(type == has_walk_array_begin) {
+        e->size += 2 +                       /* '[' and ']'         */
+            + ( cur->value.hash.count ) - 1; /* ',' between entries */
+    } else if(type == has_walk_string) {
+        e->size += 2 + size;
+        /* FIXME: handle string encoding case */
+    } else if(type == has_walk_other) {
+        /* FIXME: Nothing yet */
+    }
+
+    return 0;
+}
+
+size_t has_json_estimate(has_t *input, bool encode)
+{
+    has_json_estimate_t e;
+    e.size = 0;
+    e.encode = false;
+    return (has_walk(input, has_json_estimate_walker, &e) == 0) ? e.size : 0;
+}
+
+typedef struct has_json_serializer_t has_json_serializer_t;
+
+typedef int (*has_json_outputter) (has_json_serializer_t *s,
+                                   const char *value, size_t size);
+
+struct has_json_serializer_t {
+    has_json_outputter outputter;
+    void *pointer;
+    bool encode;
+};
+
+typedef struct {
+    char *buffer;
+    size_t size;
+    size_t current;
+    bool owner;
+} has_json_serializer_buffer_t;
+
+int has_json_serializer_buffer_outputter(has_json_serializer_t *s,
+                                         const char *value, size_t size)
+{
+    return 0;
+}
+
+int has_json_serialize_string(has_json_serializer_t *s,
+                              const char *value, size_t size)
+{
+    if(s == NULL || s->outputter == NULL) {
+        return -1;
+    }
+    
+    return (((s->outputter)(s->pointer, "\"", 1) == 0) &&
+            ((s->outputter)(s->pointer, value, size) == 0) &&
+            ((s->outputter)(s->pointer, "\"", 1) == 0)) ? 0 : -1;
+}
+
+int has_json_serializer_walker(has_t *cur, has_walk_t type, int index,
+                               const char *string, size_t size, has_t *element,
+                               void *pointer)
+{
+    has_json_serializer_t *s = pointer;
+    int r = 0;
+    if(s == NULL || s->outputter == NULL) {
+        return -1;
+    }
+
+    if(cur == NULL) {
+        r = (s->outputter)(s->pointer, "null", 4);
+    } else if(type == has_walk_hash_begin) {
+        r = (s->outputter)(s->pointer, "{", 1);
+    } else if(type == has_walk_hash_key) {
+        r = ((has_json_serialize_string(s, string, size) == 0) &&
+             ((s->outputter)(s->pointer, ":", 1) == 0)) ? 0 : -1;
+    } else if(type == has_walk_hash_value_end) {
+        if(index < cur->value.hash.count - 1) {
+            r = (s->outputter)(s->pointer, ",", 1);
+        }
+    } else if(type == has_walk_hash_end) {
+        r = (s->outputter)(s->pointer, "}", 1);
+    } else if(type == has_walk_array_begin) {
+        r = (s->outputter)(s->pointer, "[", 1);
+    } else if(type == has_walk_array_entry_end) {
+        if(index != cur->value.hash.count - 1) {
+            r = (s->outputter)(s->pointer, ",", 1);
+        }
+    } else if(type == has_walk_array_end) {
+        r = (s->outputter)(s->pointer, "]", 1);
+    } else if(type == has_walk_string) {
+        r = has_json_serialize_string(s, string, size);
+    } else if(type == has_walk_other) {
+        /* Nothing yet */
+    }
+
+    return r;
+}
+
+int has_json_serialize(has_t *input, char **output, size_t *size)
+{
+    has_json_serializer_buffer_t sb;
+    has_json_serializer_t s;
+    if(output == NULL) {
+        return -1;
+    } else if(output && *output) {
+        sb.buffer = *output;
+        if(size && *size > 0) {
+            sb.size = *size;
+        } else {
+            return -1;
+        }
+    } else {
+        if((sb.buffer = malloc(4096)) == NULL) {
+            return -1;
+        }
+        sb.size = 4096;
+    }
+    sb.current = 0;
+
+    s.outputter = has_json_serializer_buffer_outputter;
+    s.pointer = &sb;
+
+    if(has_walk(input, has_json_serializer_walker, &s) == 0) {
+        *output = sb.buffer;
+        *size = sb.current;
+    } else {
+        return -1;
+    }
+    return 0;
+}
--- has_json.h
+++ has_json.h
@@ -13,12 +13,18 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 /*
  * @brief Parses JSON-encoded text into a has_t structure
  * @param buffer NULL-terminated text
  * @return A pointer to a has_t structure or NULL in case of failure.
+ *
+ * The input string must be a valid UTF-8 encoded string. The strings
+ * in the resulting has_t structure will not be decoded.
  */
 has_t *has_json_parse(const char *buffer);
+
+int has_json_serialize(has_t *input, char **output, size_t *size);
 
 #ifdef __cplusplus
 };
