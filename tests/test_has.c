cf1ba9420a953ac66f91b6f247de70c94d2987f3
Mathias Brossard <mathias@brossard.org>
2011-03-31 15:09:02 +0200

    Add lookup test

--- tests/test_has.c
+++ tests/test_has.c
@@ -39,6 +39,13 @@ int main(int argc, char **argv)
     printf("Adding: %f\n", t2 - t1);
 
     t1 = epoch_double();
+    for(i = 0; i < j; i++) {
+        has_hash_get(h, buffer + i * 8, 8);
+    }
+    t2 = epoch_double();
+    printf("Looking up: %f\n", t2 - t1);
+
+    t1 = epoch_double();
     has_free(h);
     t2 = epoch_double();
     printf("Deleting: %f\n", t2 - t1);
